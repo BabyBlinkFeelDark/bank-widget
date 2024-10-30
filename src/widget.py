1246eaf1303f635b51b7418a8386191e04d7b384
Oleg Lomtev <lomtev.oa@gmail.com>
2024-10-30 16:37:28 +0300

    Added methods 'get_time

--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,12 @@ def mask_account_card(card_data: str) -> str:
     if " ".join(card_data.split()[:-1]) == "Счет":
         return " ".join(card_data.split()[:-1]) + " " + src.mask.get_mask_account(card_data.split()[-1])
     return " ".join(card_data.split()[:-1]) + " " + src.mask.get_mask_card_number(card_data.split()[-1])
+
+
+def get_date(current_time: str) -> str:
+    """
+    Приводит дату к формату DD.MM.YYYY
+    :param current_time: Текущее время
+    :return: Время в формате DD.MM.YYYY
+    """
+    return current_time[8:10] + "." + current_time[5:7] + "." + current_time[:4]
