800552ff687445be30d6db884f268d3d422a64d1
Oleg Lomtev <lomtev.oa@gmail.com>
2024-11-10 20:29:08 +0300

    Added a template for gitignote

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,13 @@
 def filter_by_state(in_dicts: list, state="EXECUTED") -> list:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    :param in_dicts: Список словарей, которые необходимо отфильтровать.
+    :param state: Значение ключа 'state', по которому будет производиться фильтрация.
+    По умолчанию 'EXECUTED'.
+    :return: Новый список словарей, содержащий только те словари,
+              у которых значение ключа 'state' соответствует указанному значению.
+    """
     out_dicts = []
     for d in in_dicts:
         if d.get("state") == state:
@@ -7,4 +16,12 @@ def filter_by_state(in_dicts: list, state="EXECUTED") -> list:
 
 
 def sort_by_date(in_dicts: list, sort_by="ASC") -> list:
+    """
+    Сортирует список словарей по значению ключа 'date'.
+    :param in_dicts: Список словарей, который необходимо отсортировать.
+    :param sort_by: Порядок сортировки. Может быть 'ASC' для сортировки по возрастанию
+                или 'DESC' для сортировки по убыванию. По умолчанию 'ASC'.
+    :return: Новый список словарей, отсортированный по дате (ключ 'date')
+                в указанном порядке.
+    """
     return sorted(in_dicts, key=lambda x: x["date"], reverse=[True if sort_by == "DESC" else False])
