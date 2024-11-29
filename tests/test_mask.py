1aa8a40709b4a7bff12b7a5797e502227ce46045
Oleg Lomtev <lomtev.oa@gmail.com>
2024-11-29 13:33:59 +0300

    Checking the correctness of the masking of the card

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -14,3 +14,12 @@ from src.mask import get_mask_card_number, get_mask_account
 def test_correct_data_mask_account(input_data):
     with pytest.raises(TypeError):
         get_mask_card_number(input_data)
+
+@pytest.mark.parametrize("input_data, output_data",
+    [
+    ("7000792289606361", "7000 79XX XXXX 6361"),
+    ("7790006063922861", "7790 00XX XXXX 2861"),
+                         ])
+
+def test_correct_output_mask_account(input_data, output_data):
+    assert get_mask_card_number(input_data) == output_data
