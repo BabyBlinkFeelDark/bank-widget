1ba7164dd03673af014049d65668e73b086df504
Oleg Lomtev <lomtev.oa@gmail.com>
2024-12-02 16:51:15 +0300

    I'm trying the TDD approach

--- src/generators.py
+++ src/generators.py
@@ -2,9 +2,14 @@ from typing import Any, Dict, List
 
 
 def card_number_generator(start_point, end_point):
-    result = [i for i in range(start_point,end_point)]
-    return result
+    result = [str(i) for i in range(start_point,end_point+1)]
+    map_result = list(map(real_card_number,result))
+    # return "".join((16-len(str(result[4])))*"0"+str(result[4]))
+    return map_result
 
+def real_card_number(card_list):
+     for card in card_list:
+         return "".join((16-len(card))*"0"+card)
 
 def filter_by_currency():
     return 1
