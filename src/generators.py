65c680d23ff1270fb0e40c0b73e713738ea289f2
Oleg Lomtev <lomtev.oa@gmail.com>
2024-12-02 17:35:43 +0300

    Errors have been caught

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,13 @@
 from typing import Any, Dict, List
 
 
-def card_number_generator(start_point: int , end_point : int) -> list:
 
-    result = [str(i).zfill(16) for i in range(start_point, end_point + 1)]
-    return result
+def card_number_generator(start_point: int , end_point : int) -> list:
+    if start_point < end_point:
+        result = [str(i).zfill(16) for i in range(start_point, end_point + 1)]
+        return result
+    else:
+        raise IndexError("start_point !> end_point")
 
 def filter_by_currency():
     return 1
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -11,6 +11,7 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
                 '0000000000000005']),
         (9, 10, ['0000000000000009',
                   '0000000000000010', ]),
+        ("1","2",[0]),
     ],
 )
 def test_card_generator(input_spoint,input_enpoint, expected_output):
@@ -22,10 +23,24 @@ def test_card_generator(input_spoint,input_enpoint, expected_output):
     (
         [
             ("0",1),
-            # (None)
+            ("1","2")
         ]
     ),
 )
-def test_incorrect_date(input_data):
+def test_incorrect_date(input_spoint,input_enpoint):
     with pytest.raises(TypeError):
-        card_number_generator(input_data)
+        card_number_generator(input_spoint,input_enpoint)
+
+
+@pytest.mark.parametrize(
+    "input_spoint,input_enpoint",
+    (
+        [
+            (2,1),
+            (109,9)
+        ]
+    ),
+)
+def test_incorrect_date(input_spoint,input_enpoint):
+    with pytest.raises(IndexError):
+        card_number_generator(input_spoint,input_enpoint)
