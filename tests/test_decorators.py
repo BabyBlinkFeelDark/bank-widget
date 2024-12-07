9f2d9fd66d74004cec2b2e3cf355b0057319fe12
Oleg Lomtev <lomtev.oa@gmail.com>
2024-12-07 16:56:57 +0300

    Error testing

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,9 +1,12 @@
 import pytest
-from src.decorators import log
 from src.mask import get_mask_account, get_mask_card_number
 
 def test_log(capsys):
     get_mask_card_number("1234567812345678")
     captured = capsys.readouterr()
-
     assert captured.out == 'Вызов функции: get_mask_card_number\n'
+
+
+def test_log_err(capsys):
+    with pytest.raises(Exception, match="Invalid number"):
+        get_mask_card_number("")
