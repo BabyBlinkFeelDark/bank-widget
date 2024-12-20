fb60b67668d8e65a1a9429452e3d57ffe2999d99
Oleg Lomtev <lomtev.oa@gmail.com>
2024-12-20 04:01:48 +0300

    Отработка исключений

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,15 @@ import json, os
 from src.external_api import convert
 from dotenv import load_dotenv
 
-def parser(file_path):
-    with open(file_path, 'r') as f:
-        return list(json.load(f))
+def parser(file_path: str) -> list[dict]:
+    try:
+        with open("file_path", 'r') as f:
+            return list(json.load(f))
+    except FileNotFoundError:
+        return []
 
-def transactions_summary(transaction):
+
+def transactions_summary(transaction: dict) -> float:
     if transaction.get("code")!='RUB':
         load_dotenv()
         rez = convert(transaction.get("operationAmount", {}).get("currency", {}).get("code", {}), transaction.get("operationAmount", {}).get("amount", {}), os.getenv("API_TOKEN"))
@@ -14,4 +18,20 @@ def transactions_summary(transaction):
         rez = transaction.get("operationAmount", {}).get("currency", {}).get("amount", {})
     return float(rez)
 
+print(transactions_summary({
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }))
 
+print(parser("aaaa"))
