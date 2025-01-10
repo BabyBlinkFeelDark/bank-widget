9b830c07737e587118afee3d3010e313e654e58b
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-10 17:45:08 +0300

    Типизация + обработка исключений

--- src/pars.py
+++ src/pars.py
@@ -1,14 +1,20 @@
 import csv, pandas as pd
+from typing import List, Union
 
-def pars_csv(file_path: str):
+def pars_csv(file_path: str) -> List[List[str]]:
     try:
         with open(file_path) as file:
             reader = csv.reader(file)
-            return list(reader)
-    except:
+            data = list(reader)
+            if not data:
+                raise ValueError("Файл пустой")
+            return data
+    except FileNotFoundError:
         raise FileNotFoundError("Файл не найден")
+    except Exception as e:
+        raise RuntimeError("Ошибка при работе с файлом")
 
-def pars_xlsx(file_path: str):
+def pars_xlsx(file_path: str) -> pd.DataFrame:
     try:
         reader = pd.read_excel(file_path)
         return reader.head()
