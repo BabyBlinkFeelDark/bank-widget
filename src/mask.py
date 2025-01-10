9b422d83d11f48b7e07abb2e5f197c4ce7742710
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-10 13:25:54 +0300

    get_mask_card_number обмазана логами

--- src/mask.py
+++ src/mask.py
@@ -1,4 +1,22 @@
+import logging
+import os
+
 from src.decorators import log
+
+
+card_logger = logging.getLogger('utils.parser')
+account_logger = logging.getLogger('utils.transactions_summary')
+if not os.path.isdir("../log"):
+    os.mkdir("../log")
+file_handler = logging.FileHandler('../log/utils.log', 'w')
+card_logger.addHandler(file_handler)
+account_logger.addHandler(file_handler)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(name)s %(message)s')
+file_handler.setFormatter(file_formatter)
+card_logger.addHandler(file_handler)
+account_logger.addHandler(file_handler)
+card_logger.setLevel(logging.DEBUG)
+account_logger.setLevel(logging.DEBUG)
 
 
 @log()
@@ -9,8 +27,10 @@ def get_mask_card_number(card_num: str) -> str:
     :return: Маскированный номер карты
     """
     if len(card_num) != 16:
+        card_logger.error("Несуществующий номер карты")
         raise TypeError("Invalid number")
     else:
+        card_logger.info("Карта успешно замаскирована")
         return card_num[:4] + " " + card_num[4:6] + "XX XXXX " + card_num[-4:]
 
 
