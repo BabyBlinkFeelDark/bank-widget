e9b0cea8764185345d918f5b7d60ef135736de5a
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-10 17:46:51 +0300

    Обработка исключений для метода, который работает с Эксель

--- src/pars.py
+++ src/pars.py
@@ -7,16 +7,18 @@ def pars_csv(file_path: str) -> List[List[str]]:
             reader = csv.reader(file)
             data = list(reader)
             if not data:
-                raise ValueError("Файл пустой")
+                raise ValueError(f"Файл {file_path} пустой")
             return data
     except FileNotFoundError:
-        raise FileNotFoundError("Файл не найден")
+        raise FileNotFoundError(f"Файл {file_path} не найден")
     except Exception as e:
-        raise RuntimeError("Ошибка при работе с файлом")
+        raise RuntimeError(f"Ошибка при работе с файлом {file_path}: {e}")
 
 def pars_xlsx(file_path: str) -> pd.DataFrame:
     try:
         reader = pd.read_excel(file_path)
+        if reader.empty:
+            raise ValueError("Файл пустой")
         return reader.head()
     except:
         raise FileNotFoundError("Файл не найден")
