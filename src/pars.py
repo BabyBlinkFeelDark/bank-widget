38d08e627bb871a2bec1f861b8f6235bc7e0176b
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-10 18:04:32 +0300

    PEP8+докстринги

--- src/pars.py
+++ src/pars.py
@@ -2,6 +2,14 @@ import csv, pandas as pd
 from typing import List, Union
 
 def pars_csv(file_path: str) -> List[List[str]]:
+    """
+    Считывает данные из CSV-файла и возвращает их в виде списка строк.
+
+    :param file_path: Путь к CSV-файлу.
+    :return: Данные файла в формате списка списков.
+    :raises FileNotFoundError: Если файл не найден.
+    :raises ValueError: Если файл пустой.
+    """
     try:
         with open(file_path) as file:
             reader = csv.reader(file)
@@ -14,6 +22,14 @@ def pars_csv(file_path: str) -> List[List[str]]:
 
 
 def pars_xlsx(file_path: str) -> pd.DataFrame:
+    """
+    Считывает данные из Excel-файла и возвращает первые 5 строк.
+
+    :param file_path: Путь к Excel-файлу.
+    :return: Данные файла в формате DataFrame (первые 5 строк).
+    :raises FileNotFoundError: Если файл не найден или доступ к нему невозможен.
+    :raises ValueError: Если файл пустой.
+    """
     try:
         reader = pd.read_excel(file_path)
         if reader.empty:
