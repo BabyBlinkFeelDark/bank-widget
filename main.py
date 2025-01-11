1df8e77766dff9b58cd58a47a7403809bc364de8
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-11 16:27:50 +0300

    работаю над логикой

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.widget import get_date, mask_account_card
 
-def start():
+def choose_operation():
     answers = {}
     print("""Привет! Добро пожаловать в программу работы 
         с банковскими транзакциями. 
@@ -11,34 +11,46 @@ def start():
     answers["choose_operation"] = input().lower()
     return answers
 
-
-
-
-
-
-
-def choose_operation(answers):
+def choose_status(answers):
     print("""Введите статус, по которому необходимо выполнить фильтрацию. 
                 Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING""")
     answers['choose_status'] = input().lower()
+    return answers
 
 def choose_data_sort(answers):
     print("""Отсортировать операции по дате? Да/Нет""")
     answers['choose_data_sort'] = input().lower()
+    return answers
 
 def choose_data_sort_cur(answers):
     print("""Отсортировать по возрастанию или по убыванию?""")
     answers['choose_data_sort_cur'] = input().lower()
+    return answers
 
 def choose_currency(answers):
     print("""Выводить только рублевые тразакции? Да/Нет""")
     answers['choose_currency'] = input().lower()
+    return answers
 
 def choose_filt(answers):
     print("""Отфильтровать список транзакций по определенному слову 
     в описании? Да/Нет""")
     answers['choose_filt'] = input()
+    return answers
 
+def start():
+    d = choose_operation()
 
+    if int(d['choose_operation']) < 1 or int(d['choose_operation']) > 3:
+        print("Некорректная операция")
+        start()
+    else:
+        choose_status(d)
 
-print(start())
+    while d['choose_status'].lower() not in ['executed', 'canceled', 'pending']:
+        print("Некорректный статус")
+        choose_status(d)
+
+    print(d)
+
+start()
